5250ad757e2f7dd787fbdad9dfa860bb234d8570
Hrompic <semenxx@gmail.com>
2020-05-01 15:54:05 +0300

    Update

--- start/1.c
+++ start/1.c
@@ -1,14 +1,40 @@
 #include <GL/glut.h>
-#include <GL/freeglut_ext.h>
 //#include <SFML/Graphics.hpp>
 #include <stdio.h>
+
+#ifdef ANDROID
+#include <android/log.h>
+#endif
+
+#include <GLES2/gl2.h>
+#include <GL/freeglut_ext.h>
 GLint prog;
+GLint attr2d;
+
+#ifdef ANDROID
+#define LOGI(...)  ((void)__android_log_print(ANDROID_LOG_INFO, "My glut", __VA_ARGS__))
+
+#else
+#define LOGI(...) printf(__VA_ARGS__)
+#endif
 
 void render()
 {
 	glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
     glClearColor(0,1,0,1);
-    
+	glUseProgram(prog);
+	glEnableVertexAttribArray(attr2d);
+	GLfloat triangle[] = 
+	{
+	 0.0,  0.8,
+    -0.8, -0.8,
+     0.8, -0.8,
+	};
+	glVertexAttribPointer(attr2d, 2,
+					GL_FLOAT, GL_FALSE,
+					0, triangle);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(attr2d);
     glutSwapBuffers();
 }
 int init()
@@ -24,31 +50,41 @@ int init()
 	glShaderSource(vs, 1, &vsrc, NULL);
 	glCompileShader(vs);
 	glGetShaderiv(vs, GL_COMPILE_STATUS, &comp);
-	fprintf(stderr, "Shader %d, %d\n", vs, comp );
+	LOGI("vShader %d, %d\n", vs, comp);
 	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
 	char *fsrc=
 	"#version 100\n"
-	"void main(void) { "
-	" gl_FragColor[0] = 0.0; "
-	" gl_FragColor[1] = 0.0; "
-	" gl_FragColor[2] = 1.0; "
+	"void main(void){"
+	" gl_FragColor.x = 0.0;"
+	" gl_FragColor[1] = 0.0;"
+	" gl_FragColor[2] = 1.0;"
 	"}";
 	glShaderSource(fs, 1, &fsrc, NULL);
 	glCompileShader(fs);
 	glGetShaderiv(fs, GL_COMPILE_STATUS, &comp);
-	fprintf(stderr, "Shader %d, %d\n", fs, comp );
-	
+	LOGI("fShader %d, %d\n", fs, comp );
+	char* l =  (char*)glGetString(GL_VERSION);
+	LOGI("%s",l);	
+	prog = glCreateProgram();
+	glAttachShader(prog, vs);
+	glAttachShader(prog, fs);
+	glLinkProgram(prog);
+	glGetProgramiv(prog, GL_LINK_STATUS, &link);
+	LOGI("Program:%d, %d\n", prog, link);
+	char* attr = "cord2d";
+	attr2d = glGetAttribLocation(prog, attr);
+	LOGI("Attrib: %d\n",attr2d);
 
 }
 int main(int argc, char **argv)
 {
-	glutInitContextVersion(2, 1);
+    glutInitContextVersion(2, 1);	
 	glutInit(&argc, argv);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(800, 600);
     glutInitDisplayMode(GLUT_RGBA| GLUT_DEPTH| GLUT_DOUBLE);
     glutCreateWindow("Wimdow");
-	init();
+    init();
     glutDisplayFunc(render);
     glutMainLoop();	
 }
